2b894b02b7dae1f7b268d521c3aeeded13e3bdf8
Dori <sky980221@naver.com>
2024-07-27 18:15:56 +0900

    initial commit

---
+++ src/main/java/tipit/tipit/TipitApplication.java
@@ -0,0 +1,13 @@
+package tipit.tipit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tipit/tipit/TipitApplicationTests.java
@@ -0,0 +1,13 @@
+package tipit.tipit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
